59370b9ead9cc61c10982bbed7da09d8472501cb
okputu-e <immanuelokputu94@gmail.com>
2022-05-16 08:04:20 +0100

    updated ui added viewPassword functionality

--- index.js
+++ index.js
@@ -166,3 +166,15 @@ function validatePhoneNumber(number) {
 
   return re.test(number);
 }
+
+//view password
+const viewPassword = document.getElementById("eyePassword");
+
+viewPassword.addEventListener("click", function (e) {
+  // toggle the type attribute
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+  // toggle the eye slash icon
+  this.classList.toggle("fa-eye-slash");
+});
